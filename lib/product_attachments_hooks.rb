01d4393635d467caa0b048d4c11485a6cb026c0e
Pierre <pierre.basile@gmail.com>
2011-07-05 23:07:15 +0200

    switch to deface

--- lib/product_attachments_hooks.rb
+++ lib/product_attachments_hooks.rb
@@ -1,5 +1,13 @@
 class ProductAttachmentsHooks < Spree::ThemeSupport::HookListener
-  insert_after :admin_product_tabs, "admin/shared/product_downloadables_tab"
+  Deface::Override.new(:virtual_path => "admin/shared/_product_tabs",
+                       :name => "converted_admin_product_tabs_52969786",
+                       :insert_bottom => "[data-hook='admin_product_tabs'], #admin_product_tabs[data-hook]",
+                       :partial => "admin/shared/product_downloadables_tab",
+                       :disabled => false)
 
-  insert_before :cart_form,  'downloadable_links'
+  Deface::Override.new(:virtual_path => "products/show",
+                       :name => "converted_cart_form_625298506",
+                       :insert_bottom => "[data-hook='cart_form'], #cart_form[data-hook]",
+                       :partial => "products/downloadable_links",
+                       :disabled => false)
 end
